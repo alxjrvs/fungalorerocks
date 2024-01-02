1a3c25ad27a426b275b54dc916b9558bbcc1fdfa
alxjrvs@gmail.com <alxjrvs@gmail.com>
2024-01-02 18:35:21 -0500

    basics

---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+	export const submit = () => {};
+</script>
+
+<h1>Fungalore</h1>
+<h2>He heard your wish.</h2>
+
+<p>
+	Take a moment to compose a wish. When you are ready, close your eyes and hit "submit" or "Enter".
+</p>
+<input type="text" />
+<button on:click={submit}>Submit</button>
+<p>This text of these wishes are not saved, stored, or transmitted.</p>
+<p>Wishes made will be heard by Fungalore, be they here or elsewhere.</p>
+<p>This is merely an opportunity to reflect on our needs and the needs of our community.</p>

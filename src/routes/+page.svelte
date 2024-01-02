c3e6950a31a8c21304c6645da2f39e130c7f2977
alxjrvs@gmail.com <alxjrvs@gmail.com>
2024-01-02 18:43:32 -0500

    Huh, this is wildly easy.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,14 @@
 <script>
-	export const submit = () => {};
+	$: wish = '';
+	$: wished = false;
+	export const submit = () => {
+		wish = '';
+		wished = true;
+
+		setTimeout(() => {
+			wished = false;
+		}, 5000);
+	};
 </script>
 
 <h1>Fungalore</h1>
@@ -8,8 +17,14 @@
 <p>
 	Take a moment to compose a wish. When you are ready, close your eyes and hit "submit" or "Enter".
 </p>
-<input type="text" />
-<button on:click={submit}>Submit</button>
+
+{#if wished}
+	<h3>Heard, Chef.</h3>
+{:else}
+	<input type="text" bind:value={wish} />
+	<button on:click={submit}>Submit</button>
+{/if}
+
 <p>This text of these wishes are not saved, stored, or transmitted.</p>
 <p>Wishes made will be heard by Fungalore, be they here or elsewhere.</p>
-<p>This is merely an opportunity to reflect on our needs and the needs of our community.</p>
+<p>This is merely an opportunity to reflect on your needs and the needs of y our community.</p>
